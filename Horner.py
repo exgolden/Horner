9fae5228dc8d18b7235abc5b4d9fc2d22b36cecc
Isai <isai_alejandro@outlook.com>
2022-07-05 16:13:43 -0500

    Update Horner.py
    corecion de string

--- Horner.py
+++ Horner.py
@@ -1,6 +1,6 @@
 import numpy as np
 print("Este programa evalua polinomios")
-Coeff=str(input("Ingrese los coeficienets del polinomio -incluidos los elementos cero-: a1,a2,...,an: "))
+Coeff=str(input("Ingrese los coeficienets del polinomio -incluidos los elementos cero-: an,an-1,...,a2,a1: "))
 VAL=float(input("Ingrese el valor a evaluar: "))
 Coeff=np.array(Coeff.split(","), dtype=float)
 K=Coeff[0]
